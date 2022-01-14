c40bfd1b6f7f400f6d4c06f3894da4bc6905b41b
Adam Bryant <32221248+lumpylumpylumpy@users.noreply.github.com>
2022-01-14 15:56:23 +1100

    Add files via upload

--- scripting.js
+++ scripting.js
@@ -13,3 +13,16 @@ var x = setInterval(function() {
   
   document.getElementById("dots").innerHTML = d;
 }, 1000);
+
+
+// Get today's time
+var sec = new Date().getSeconds();
+
+// Display the result
+var d = "";
+if (sec % 4 == 0) { d = "   "; }
+if (sec % 4 == 1) { d = ".  "; }
+if (sec % 4 == 2) { d = ".. "; }
+if (sec % 4 == 3) { d = "..."; }
+
+document.getElementById("dots").innerHTML = d;
