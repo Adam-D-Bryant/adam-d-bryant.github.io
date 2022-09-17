68aa2a9083fb12f509dd1990013afac4677870a8
Adam Bryant <32221248+lumpylumpylumpy@users.noreply.github.com>
2022-09-17 23:46:27 +1000

    Initial Upload for Christmas 2022
    Includes at least 1 item in all 4 main pages. Uses Flexbox this time and should be responsive.

---
+++ js/homepage_want_to.js
@@ -0,0 +1,11 @@
+$("#clothes").hover(function () { $("#wantTo").html(" wear something?"); $("#wantTo").css("color", "rgb(49, 170, 49)"); },
+        function () { $("#wantTo").html("..."); $("#wantTo").css("color", "white"); });
+
+$("#videoGames").hover(function () { $("#wantTo").html(" play something?"); $("#wantTo").css("color", "rgb(49, 170, 49)"); },
+        function () { $("#wantTo").html("..."); $("#wantTo").css("color", "white"); });
+
+$("#food").hover(function () { $("#wantTo").html(" eat something?"); $("#wantTo").css("color", "rgb(49, 170, 49)"); },
+        function () { $("#wantTo").html("..."); $("#wantTo").css("color", "white"); });
+
+$("#misc").hover(function () { $("#wantTo").html(" use something?"); $("#wantTo").css("color", "rgb(49, 170, 49)"); },
+        function () { $("#wantTo").html("..."); $("#wantTo").css("color", "white"); });
