6478726a82d8abee57030bbaa4fbe25a53752b54
Adam-D-Bryant <adam.bryant@outlook.com.au>
2024-04-09 17:18:42 +1000

    Updated navbar to use better setup. Plan to continue development without directly specifying flex properties (e.g., flex-2, flex-centre).

---
+++ wishlist/js/navbar.js
@@ -0,0 +1,31 @@
+const toggleButton = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".nav-menu");
+const items = document.querySelectorAll(".nav-item");
+
+function toggleMenu() {
+
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggleButton.querySelector(".nav-link").innerHTML = "<i class='nav-toggle-icon fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        toggleButton.querySelector(".nav-link").innerHTML = "<i class='nav-toggle-icon fas fa-times'></i>";
+    }
+}
+
+toggleButton.addEventListener("click", toggleMenu, false);
+
+function closeMenu(e) {
+
+    if (menu.classList.contains("active")) {
+
+        let isClickInside = false;
+
+        for (let i=0; i < e.target.classList.length; i++) 
+            if ((e.target.classList[i]).indexOf("nav") != -1) isClickInside = true;
+        
+        if (!isClickInside)
+            toggleMenu();
+    }
+}
+document.addEventListener("click", closeMenu, false);  
