f3b3335a2c4c5da5efd1fd2c2ce5239cbe5725c7
Adam-D-Bryant <adam.bryant@outlook.com.au>
2024-04-19 19:11:53 +1000

    First verison of final site. Ready to move to PROD.

--- wishlist/js/homepage.js
+++ wishlist/js/homepage.js
@@ -3,6 +3,8 @@
 const BIRTHDATE = new Date("2024-08-28T00:00:00");
 const INTERVAL_ID = setInterval(updateTimer, 1000);
 
+let timerHover = false;
+
 function updateTimer() {
     let now, diff;
     let sec, min, hr, day;
@@ -12,7 +14,15 @@ function updateTimer() {
 
     if (diff <= 0) {
         if (INTERVAL_ID) clearInterval(INTERVAL_ID);
-        document.getElementById("timer").innerHTML = "HAPPY BIRTHDAY!";
+        document.getElementById("timerD").innerHTML = "HAPPY BIRTHDAY!";
+        document.getElementById("timerH").innerHTML = ``;
+        document.getElementById("timerM").innerHTML = ``;
+        document.getElementById("timerS").innerHTML = ``;
+
+        document.getElementById("timerDExtra").innerHTML = ``;
+        document.getElementById("timerHExtra").innerHTML = ``;
+        document.getElementById("timerMExtra").innerHTML = ``;
+        document.getElementById("timerSExtra").innerHTML = ``;
         return;
     }
 
@@ -21,5 +31,30 @@ function updateTimer() {
     hr = (Math.floor(diff / 3600000) % 24);
     day = Math.floor(diff / 86400000);
 
-    document.getElementById("timer").innerHTML = `${day<=9 ? "0":""}${day}:${hr<=9 ? "0":""}${hr}:${min<=9 ? "0":""}${min}:${sec<=9 ? "0":""}${sec}`;
+    document.getElementById("timerD").innerHTML = `${day<=9 ? "0":""}${day}`;
+    document.getElementById("timerH").innerHTML = `${hr<=9 ? "0":""}${hr}`;
+    document.getElementById("timerM").innerHTML = `${min<=9 ? "0":""}${min}`;
+    document.getElementById("timerS").innerHTML = `${sec<=9 ? "0":""}${sec}`;
+
+    document.getElementById("timerDExtra").innerHTML = `${timerHover ? ` Day${day!=1 ? "s":""}, `:`:`}`;
+    document.getElementById("timerHExtra").innerHTML = `${timerHover ? ` Hour${hr!=1 ? "s":""}, `:`:`}`;
+    document.getElementById("timerMExtra").innerHTML = `${timerHover ? ` Minute${min!=1 ? "s":""}, `:`:`}`;
+    document.getElementById("timerSExtra").innerHTML = `${timerHover ? ` Second${sec!=1 ? "s":""}`:``}`;
 }
+
+function setHover(v) {
+    timerHover = v;
+
+    now = new Date();
+    diff = BIRTHDATE-now;
+
+    if (diff <= 0 || document.getElementById("timerD").innerHTML === "[Loading...]") return;
+
+    document.getElementById("timerDExtra").innerHTML = `${timerHover ? " Days, ":":"}`;
+    document.getElementById("timerHExtra").innerHTML = `${timerHover ? " Hours, ":":"}`;
+    document.getElementById("timerMExtra").innerHTML = `${timerHover ? " Minutes, ":":"}`;
+    document.getElementById("timerSExtra").innerHTML = `${timerHover ? " Seconds":""}`;
+}
+
+document.getElementById("subtitle").addEventListener("mouseover", function() { setHover(true); });
+document.getElementById("subtitle").addEventListener("mouseout", function() { setHover(false); });
--- wishlist/js/navbar.js
+++ wishlist/js/navbar.js
@@ -15,16 +15,37 @@ function toggleMenu() {
 toggleButton.addEventListener("click", toggleMenu, false);
 
 function closeMenu(e) {
-
     if (menu.classList.contains("active")) {
-
         let isClickInside = false;
-
         for (let i=0; i < e.target.classList.length; i++) 
             if ((e.target.classList[i]).indexOf("nav") != -1) isClickInside = true;
-        
         if (!isClickInside)
             toggleMenu();
     }
 }
-document.addEventListener("click", closeMenu, false);  
+document.addEventListener("click", closeMenu, false);
+
+function highlight(e, en, col) {
+    let parent = e.parentNode;
+    let item = parent.parentNode;
+    if (en && !item.classList.contains(`highlight-${col}`)) 
+        item.classList.add(`highlight-${col}`);
+    else if (!en && item.classList.contains(`highlight-${col}`)) 
+        item.classList.remove(`highlight-${col}`);
+}
+
+//Clothes
+document.querySelector("#nav-link-clothes").addEventListener("mouseenter", function(e) { highlight(e.target, true, "g"); }, false);
+document.querySelector("#nav-link-clothes").addEventListener("mouseleave", function(e) { highlight(e.target, false, "g"); }, false);
+
+//Food
+document.querySelector("#nav-link-food").addEventListener("mouseenter", function(e) { highlight(e.target, true, "o"); }, false);
+document.querySelector("#nav-link-food").addEventListener("mouseleave", function(e) { highlight(e.target, false, "o"); }, false);
+
+//Games
+document.querySelector("#nav-link-games").addEventListener("mouseenter", function(e) { highlight(e.target, true, "b"); }, false);
+document.querySelector("#nav-link-games").addEventListener("mouseleave", function(e) { highlight(e.target, false, "b"); }, false);
+
+//Misc
+document.querySelector("#nav-link-misc").addEventListener("mouseenter", function(e) { highlight(e.target, true, "p"); }, false);
+document.querySelector("#nav-link-misc").addEventListener("mouseleave", function(e) { highlight(e.target, false, "p"); }, false);
