4c3c75c0ba2db4c2874f1e3573302bf1208ac461
Adam Bryant <adam.bryant@outlook.com.au>
2025-03-19 11:55:48 +1100

    Made main page with timer.

--- wishlist/js/timer.js
+++ wishlist/js/timer.js
@@ -1,17 +1,36 @@
-const birthdate = new Date(2025, 2, 20);
+const birthdate = new Date(2025, 7, 28);
+var interval;
 
 function refreshTimer() {
     var now = new Date();
     var diff = birthdate - now;
 
-    var sec = (diff / 1000) % 60;
-    var min = (diff / 60000) % 60;
-    var hour = (diff / 3600000) % 24;
-    var day = diff / 86400000;
+    if (diff < 0) {
+        document.getElementById("timerHB").classList.remove("hideTimerSection");
+        document.getElementById("timerD").classList.add("hideTimerSection");
+        document.getElementById("timerH").classList.add("hideTimerSection");
+        document.getElementById("timerM").classList.add("hideTimerSection");
+        document.getElementById("timerS").classList.add("hideTimerSection");
+        clearInterval(interval);
+        return;
+    }
 
-    document.getElementById("timer").innerHTML = `${day} : ${hour} : ${min} : ${sec}`;
+    var sec = Math.max(Math.floor(diff / 1000) % 60, 0);
+    var min = Math.max(Math.floor(diff / 60000) % 60, 0);
+    var hour = Math.max(Math.floor(diff / 3600000) % 24, 0);
+    var day = Math.max(Math.floor(diff / 86400000), 0);
+
+    var secText = `${sec<10?"0":""}${sec}`;
+    var minText = `${min<10?"0":""}${min}`;
+    var hourText = `${hour<10?"0":""}${hour}`;
+    var dayText = `${day<10?"0":""}${day}`;
+
+    document.getElementById("timerDVal").innerHTML = `${dayText}`;
+    document.getElementById("timerHVal").innerHTML = `${hourText}`;
+    document.getElementById("timerMVal").innerHTML = `${minText}`;
+    document.getElementById("timerSVal").innerHTML = `${secText}`;
 }
 
-setInterval(refreshTimer, 1000);
+interval = setInterval(refreshTimer, 1000);
 
 refreshTimer();
