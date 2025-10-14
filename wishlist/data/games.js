4961b3d69a2ba243423667a04de8d74728b84d83
Adam Bryant <adam.bryant@outlook.com.au>
2025-10-15 09:41:14 +1100

    Added Pikmin games for Switch. Slight adjustments to the logo font size.

--- wishlist/data/games.js
+++ wishlist/data/games.js
@@ -15,6 +15,18 @@ itemData = [
         "imageFileName": "./games/games_witsend.jpg",
         "imageAlt": "Wit's End",
         "description": `An older game, Wit's End is a challenging trivia and lateral thinking game where players compete to be the first to reach the center of the board. Movement depends on answering questions, analogies, riddles, definitions, odd facts, logic puzzles, and "sequence" challenges.`
+    },
+    {
+        "title": "PIKMIN 1+2 (SWITCH)",
+        "imageFileName": "./games/pikmin12.jpg",
+        "imageAlt": "Wit's End",
+        "description": `2 classic GameCube games brought to the Switch! Play as Captain Olimar and a capable cast of red, blue, and yellow Pikmin on a dire mission across a larger-than-life planet. You must guide your loveably leafy companions through obstacles, in battle, and when collecting missing ship parts. Your oxygen will only last 30 days, but the Pikmin provide a glimmer of hope that you may make it home.`
+    },
+    {
+        "title": "PIKMIN 3 DELUXE (SWITCH)",
+        "imageFileName": "./games/pikmin3.jpg",
+        "imageAlt": "Wit's End",
+        "description": `Originally a Wii U game, Pikmin 3 Deluxe lets you traverse a strange world and save your planet, command a capable crop of 5 different types of Pikmin to strategically overcome obstacles, defeat creatures, and find food for your famished home planet!`
     }
 ];
 
